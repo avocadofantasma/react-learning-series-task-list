c2b982c92431ba42b1968d2f42a304dff31a0f55
Carlos Estrada <caestrada>
2019-01-29 13:07:17 -0600

    build

---
+++ build/precache-manifest.6cbd59e7cd0ec61ba8608d78fd2a0eac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66c7cb690fce7d935482",
+    "url": "/static/js/main.66c7cb69.chunk.js"
+  },
+  {
+    "revision": "33e7583871af5899b20c",
+    "url": "/static/js/1.33e75838.chunk.js"
+  },
+  {
+    "revision": "66c7cb690fce7d935482",
+    "url": "/static/css/main.394c984f.chunk.css"
+  },
+  {
+    "revision": "33e7583871af5899b20c",
+    "url": "/static/css/1.53483603.chunk.css"
+  },
+  {
+    "revision": "146da9b5b75d62a3ada2a6ac85927f27",
+    "url": "/index.html"
+  }
+];
